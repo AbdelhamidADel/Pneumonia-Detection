c020de37df0023879d98cd2d41954e4ebb0733d0
Abdelhamid Adel <104658866+AbdelhamidADel@users.noreply.github.com>
2022-10-25 20:53:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,52 +11,6 @@ selected = option_menu(None, ['Detection', 'About'],
     icons=['house',"list-task"], 
     menu_icon="cast", default_index=0, orientation="horizontal")
 
-selected_styl="""<style>
-    ul{
-  background: #FF4742;
-  border: 1px solid #FF4742;
-  border-radius: 6px;
-  box-shadow: rgba(0, 0, 0, 0.1) 1px 2px 4px;
-  box-sizing: border-box;
-  color: #FFFFFF;
-  cursor: pointer;
-  display: inline-block;
-  font-family: nunito,roboto,proxima-nova,"proxima nova",sans-serif;
-  font-size: 16px;
-  font-weight: 800;
-  line-height: 16px;
-  min-height: 40px;
-  outline: 0;
-  padding: 12px 14px;
-  text-align: center;
-  text-rendering: geometricprecision;
-  text-transform: none;
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-  vertical-align: middle;
-  margin: 0;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-}
-
-ul:hover,
-ul:active {
-  background-color: initial;
-  background-position: 0 0;
-  color: #FF4742;
-}
-
-ul:active {
-  opacity: .5;
-}
-
-    </style>"""
-st.markdown(selected_styl, unsafe_allow_html=True) 
-
 #----------------------------------------------------------------
 
 def get_base64(bin_file):
